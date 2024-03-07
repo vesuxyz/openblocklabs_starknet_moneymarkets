d018f0f3e53d0007591ce97001103da6b3ed7224
9oelM <hj923@hotmail.com>
2024-03-07 12:00:45 +0800

    feat: store address as str

--- zklend/function.py
+++ zklend/function.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Dict, List, Union
 from starknet_py.contract import Contract
 from starknet_py.net.full_node_client import FullNodeClient
+from starknet_py.cairo.felt import decode_shortstring
 import pandas as pd
 import json
 
@@ -10,31 +11,31 @@ Z_TOKEN_ABI: List = json.load(open('./zklend/ztoken.abi.json'))
 MARKET_ABI: List = json.load(open('./zklend/market.abi.json'))
 
 NODE_URL = "https://starknet-mainnet.public.blastapi.io"
-MARKET = 0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05
+MARKET = "0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05"
 
 assets: List[Dict[str, Union[str, int]]] = [{
     "symbol": "STRK",
     "token_decimals": 18,
-    "underlying": 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d,
-    "z_token": 0x06d8fa671ef84f791b7f601fa79fea8f6ceb70b5fa84189e3159d532162efc21,
+    "underlying": "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+    "z_token": "0x06d8fa671ef84f791b7f601fa79fea8f6ceb70b5fa84189e3159d532162efc21",
 },
 {
     "symbol": "ETH",
     "token_decimals": 18,
-    "underlying": 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,
-    "z_token": 0x01b5bd713e72fdc5d63ffd83762f81297f6175a5e0a4771cdadbc1dd5fe72cb1,
+    "underlying": "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+    "z_token": "0x01b5bd713e72fdc5d63ffd83762f81297f6175a5e0a4771cdadbc1dd5fe72cb1",
 },
 {
     "symbol": "USDC",
     "token_decimals": 6,
-    "underlying": 0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8,
-    "z_token": 0x047ad51726d891f972e74e4ad858a261b43869f7126ce7436ee0b2529a98f486,
+    "underlying": "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+    "z_token": "0x047ad51726d891f972e74e4ad858a261b43869f7126ce7436ee0b2529a98f486",
 },
 {
     "symbol": "USDT",
     "token_decimals": 6,
-    "underlying": 0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8,
-    "z_token": 0x00811d8da5dc8a2206ea7fd0b28627c2d77280a515126e62baa4d78e22714c4a,
+    "underlying": "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
+    "z_token": "0x00811d8da5dc8a2206ea7fd0b28627c2d77280a515126e62baa4d78e22714c4a",
 }]
 
 client = FullNodeClient(node_url=NODE_URL)
@@ -77,20 +78,23 @@ async def main():
     df.to_csv('output_zklend.csv', index=False)
 
 async def get_data(asset):
-    supply = await get_supply(asset["z_token"])
-    debt = await get_debt(asset["underlying"])
+    z_token_int = int(asset["z_token"], 16)
+    underlying_int = int(asset["underlying"], 16)
+    supply = await get_supply(z_token_int)
+    debt = await get_debt(underlying_int)
     block_height = await client.get_block_number()
+    lending_accumulator = await get_lending_accumulator(underlying_int)
     return {
         "protocol": "zkLend",
         "date": get_today(),
-        "market": asset["z_token"],
+        "market": asset["underlying"],
         "tokenSymbol": asset["symbol"],
         "supply_token": supply,
         "borrow_token": debt,
         "net_supply_token": supply - debt,
         "non_recursive_supply_token": 0,
         "block_height": block_height,
-        "lending_index_rate": get_lending_accumulator(asset["underlying"])
+        "lending_index_rate": lending_accumulator
     }
 
 if __name__ == "__main__":
