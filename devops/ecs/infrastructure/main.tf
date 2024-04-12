bff09538a2fd9c18f8286478240c8ad748f2cec6
willwooten <wwootencfc@gmail.com>
2024-04-12 14:12:34 -0500

    add sui bucket

--- devops/ecs/infrastructure/main.tf
+++ devops/ecs/infrastructure/main.tf
@@ -175,6 +175,8 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
       "arn:aws:s3:::starknet-openblocklabs/*",
       "arn:aws:s3:::mode-openblocklabs",
       "arn:aws:s3:::mode-openblocklabs/*",
+      "arn:aws:s3:::sui-openblocklabs",
+      "arn:aws:s3:::sui-openblocklabs/*",
     ]
   }
 }
