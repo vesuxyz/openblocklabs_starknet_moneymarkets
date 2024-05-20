f6e47e0985546b9e1c9892417a9046d2c623217e
0xSacha <90143060+0xSacha@users.noreply.github.com>
2024-05-20 18:51:57 +0200

    fix(sdai): add stablecoin

--- protocols/nimbora/lend/function.py
+++ protocols/nimbora/lend/function.py
@@ -4,11 +4,23 @@ from starknet_py.contract import Contract
 from starknet_py.net.full_node_client import FullNodeClient
 from datetime import datetime
 import pandas as pd
+from decimal import Decimal
 
 mTokenAbi = json.load(open('./openblocklabs_starknet_moneymarkets/protocols/nimbora/lend/mToken.abi.json'))
+PRAGMA_ADAPTER_ABI = json.load(open("./openblocklabs_starknet_moneymarkets/protocols/zklend/pragma_adapter.abi.json"))
 
-# Returns row for each token
-# Contains supply and lending index
+
+async def get_pragma_dai_price(block_height, client):
+    contract = Contract(
+        address=int('0x05d1bc06ca368cc451f63b20bc12bd2299a4ae7776f4dcf977723839bef311a0', 16),
+        abi=PRAGMA_ADAPTER_ABI,
+        provider=client,
+    )
+    (uint_raw_price,) = await contract.functions["get_price"].call(
+        block_number=block_height
+    )
+    decimal_price = Decimal(uint_raw_price) / Decimal(f"1e8")
+    return decimal_price
 
 
 async def get_token_info(tokenInfo, provider):
@@ -36,7 +48,26 @@ async def get_token_info(tokenInfo, provider):
         "lending_index_rate": lending_rate
     }
 
-# Define functions
+
+
+async def combine_stables(data, client):
+    stable = "DAI"
+    block = data[0]["block_height"]
+    dai_price = await get_pragma_dai_price(block, client)    
+    supply_token = dai_price * Decimal(data[0]["supply_token"])
+
+    return {
+        "protocol": "Nimbora",
+        "date": data[0]["date"],
+        "market": "0x0stable",
+        "tokenSymbol": "STB",
+        "supply_token": supply_token,
+        "borrow_token": 0,
+        "net_supply_token": supply_token,
+        "non_recursive_supply_token": supply_token,
+        "block_height": block,
+        "lending_index_rate": 1,
+    }
 
 
 async def main():
@@ -52,6 +83,9 @@ async def main():
         coroutines = [get_token_info(tokenInfo, provider)
                       for tokenInfo in tokens]
         gathered_results = await asyncio.gather(*coroutines)
+
+        stables_combined_row = await combine_stables(gathered_results, provider)
+        gathered_results.append(stables_combined_row)
 
         df = pd.DataFrame(gathered_results)
 
