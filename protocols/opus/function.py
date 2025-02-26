acdffe2a09fb15698fddff2c8b48b3b7849a8eef
tserg <8017125+tserg@users.noreply.github.com>
2025-02-26 15:58:40 +0800

    change wsteth address

--- protocols/opus/function.py
+++ protocols/opus/function.py
@@ -56,11 +56,11 @@ COLLATERAL: List[Dict[str, Union[str, int]]] = [
         "gate": "0x031a96fe18fe3fdab28822c82c81471f1802800723c8f3e209f1d9da53bc637d",
     },
     {
-        "asset": "0x042b8f0484674ca266ac5d08e4ac6a3fe65bd3129795def2dca5c34ecc5f96d2",
+        "asset": "0x0057912720381af14b0e5c87aa4718ed5e527eab60b3801ebf702ab09139e38b",
         "name": "Starknet Wrapped Staked Ether",
         "symbol": "wstETH",
         "decimals": 18,
-        "gate": "0x02d1e95661e7726022071c06a95cdae092595954096c373cde24a34bb3984cbf",
+        "gate": "0x03dc297a3788751d6d02acfea1b5dcc21a0eee1d34317a91aea2fbd49113ea58",
     },
     {
         "asset": "0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac",
